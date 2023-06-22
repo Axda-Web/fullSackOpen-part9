c0304af9cb4b2fd4b7bb2f10e4f02fa0dc1536f5
Alyx DARENNE <axda.web@gmail.com>
2023-06-22 21:45:35 +0200

    part9/ex5: Add WebBMI route

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+import { calculateBmi, parseBimCalculatorArguments } from "./bmiCalculator";
 import express from "express";
 const app = express();
 
@@ -5,6 +6,33 @@ app.get("/hello", (_req, res) => {
   res.send("Hello Full Stack!");
 });
 
+app.get("/bmi", (req, res) => {
+  const height = req.query.height;
+  const weight = req.query.weight;
+
+  if (!height || !weight) {
+    res.status(400).json({ error: "height and weight are required" });
+    return;
+  }
+
+  try {
+    const { parsedHeight, parsedWeight } = parseBimCalculatorArguments(
+      height as string,
+      weight as string
+    );
+    const bmi = calculateBmi(parsedHeight, parsedWeight);
+    res.status(200).json({
+      height,
+      weight,
+      bmi,
+    });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      res.status(400).json({ error: error.message });
+    }
+    console.log("Something went wrong");
+  }
+});
 const PORT = 3003;
 
 app.listen(PORT, () => {
