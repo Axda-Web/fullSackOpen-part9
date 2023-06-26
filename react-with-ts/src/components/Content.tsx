4f23dc42b27f10be7bf8683af9f9711193efc508
Alyx DARENNE <axda.web@gmail.com>
2023-06-26 16:09:56 +0200

    part9/ex15: Add type narrowing

--- react-with-ts/src/components/Content.tsx
+++ react-with-ts/src/components/Content.tsx
@@ -1,13 +1,12 @@
 import { CoursePart } from "../types";
+import Part from "./Part";
 
 export type ContentProps = {
   courseParts: CoursePart[];
 };
 const Content = ({ courseParts }: ContentProps) => {
-  const coursePartsView = courseParts.map(({ name, exerciseCount }, index) => (
-    <p key={index}>
-      {name} {exerciseCount}
-    </p>
+  const coursePartsView = courseParts.map((coursePart, index) => (
+    <Part key={index} coursePart={coursePart} />
   ));
   return <div>{coursePartsView}</div>;
 };
