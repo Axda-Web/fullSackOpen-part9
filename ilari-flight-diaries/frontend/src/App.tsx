f00ccfa27c0a15ecc2ce790c53a1517c4efbefc4
Alyx DARENNE <axda.web@gmail.com>
2023-06-27 10:25:05 +0200

    part9/ex16: Add Diaries view

--- ilari-flight-diaries/frontend/src/App.tsx
+++ ilari-flight-diaries/frontend/src/App.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import diaryService from "./services/diaryService";
+import { Diaries } from "./types";
+
+import DiariesList from "./components/DiariesList";
 
 function App() {
-  return <div>APP</div>;
+  const [diaries, setDiaries] = useState<Diaries>([]);
+  useEffect(() => {
+    const abortController = new AbortController();
+    diaryService.getAll(abortController.signal).then((data) => {
+      setDiaries(data);
+    });
+    return () => {
+      abortController.abort();
+    };
+  }, []);
+
+  return (
+    <>
+      <DiariesList diaries={diaries} />
+    </>
+  );
 }
 
 export default App;
